f538100e3bf22d4efa6d2cf9a37b5806632bf4ca
SimzMapisa <simbarashe795@gmail.com>
2021-12-15 16:38:55 +0200

    add browser router - router and routes for navigation between the screens

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './App.css';
-import HomeScreen from './HomeScreen'
+import HomeScreen from './screens/HomeScreen';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
+
+  const user = true;
+
   return (
     <div className="app">
-      <HomeScreen />
+      <Router>
+
+        {!user ? (
+          <h1>Login</h1>
+        ) : (
+          <Routes>
+            <Route exact path="/" element={<HomeScreen />} />
+          </Routes>
+        )}
+
+      </Router>
     </div>
   );
 }
